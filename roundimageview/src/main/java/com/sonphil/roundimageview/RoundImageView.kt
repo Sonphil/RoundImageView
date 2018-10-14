9dd471dd42a021558e0ba7efe1b64ecca8032fd4
Thanh-Son-Philippe Lam <thanh-son-philippe.lam.1@ens.etsmtl.ca>
2018-10-13 21:22:15 -0400

    Use AppCompatImageView instead of ImageView

--- roundimageview/src/main/java/com/sonphil/roundimageview/RoundImageView.kt
+++ roundimageview/src/main/java/com/sonphil/roundimageview/RoundImageView.kt
@@ -2,10 +2,10 @@ package com.sonphil.roundimageview
 
 import android.content.Context
 import android.graphics.Outline
+import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewOutlineProvider
-import android.widget.ImageView
 
 /**
  * Created by Sonphil on 13-10-18.
@@ -15,7 +15,7 @@ class RoundImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr) {
+) : AppCompatImageView(context, attrs, defStyleAttr) {
     init {
         outlineProvider = object : ViewOutlineProvider() {
             override fun getOutline(view: View, outline: Outline) {
